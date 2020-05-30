4c84bb430870f672073e3939d123b46d55182e63
laffachan <leilasafsaf6@gmail.com>
2020-05-30 15:55:38 +0200

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { HashRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Revision from "./Revision";
 import Practice from "./Practice";
 import { hiragana, katakana } from "./kana";
