356d7a8b387b95018169a6531b27b3e651df1451
laffachan <45162759+laffachan@users.noreply.github.com>
2020-06-04 21:57:54 +0200

    Update precache-manifest.1bac0bf950345a6feac9d17a8b19c4df.js

--- precache-manifest.1bac0bf950345a6feac9d17a8b19c4df.js
+++ precache-manifest.1bac0bf950345a6feac9d17a8b19c4df.js
@@ -1,22 +1,22 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "b693fdbbd47a2aeea00557b3cb18937d",
-    "url": "/KANA-Practice/index.html"
+    "url": "/kana-practice/index.html"
   },
   {
     "revision": "af4c9a96aee0a6c98b62",
-    "url": "/KANA-Practice/static/js/2.b9bce78c.chunk.js"
+    "url": "/kana-practice/static/js/2.b9bce78c.chunk.js"
   },
   {
     "revision": "cb4293a028d0175415ddb31dc1aaaefb",
-    "url": "/KANA-Practice/static/js/2.b9bce78c.chunk.js.LICENSE.txt"
+    "url": "/kana-practice/static/js/2.b9bce78c.chunk.js.LICENSE.txt"
   },
   {
     "revision": "d7e60628f204894049bf",
-    "url": "/KANA-Practice/static/js/main.e2a48155.chunk.js"
+    "url": "/kana-practice/static/js/main.e2a48155.chunk.js"
   },
   {
     "revision": "2d54a10dad253b24a7d0",
-    "url": "/KANA-Practice/static/js/runtime-main.9fb2b1d2.js"
+    "url": "/kana-practice/static/js/runtime-main.9fb2b1d2.js"
   }
 ]);
